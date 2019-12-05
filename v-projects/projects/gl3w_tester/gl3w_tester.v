1bf1fbf5f5afbf9398207ca010ca96a651f48f77
Mike <mike@prime31.com>
2019-12-04 23:32:26 -0800

    more progress

--- v-projects/projects/gl3w_tester/gl3w_tester.v
+++ v-projects/projects/gl3w_tester/gl3w_tester.v
@@ -1,7 +1,19 @@
 import prime31.sdl2
-import prime31.gl3w
+import prime31.gl3w as gl
+
+struct AppState {
+mut:
+	program u32
+	vert_pos_loc int
+	vbo u32
+	ibo u32
+
+	tri_vbo u32
+	vao u32 = u32(0)
+}
 
 fn main() {
+	mut state := &AppState{}
 	C.SDL_Init(C.SDL_INIT_VIDEO | C.SDL_INIT_JOYSTICK)
 
     C.SDL_GL_SetAttribute(C.SDL_GL_CONTEXT_FLAGS, C.SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG)
@@ -22,6 +34,9 @@ fn main() {
 
 	C.gl3wInit()
 
+	state.create_shader()
+	state.create_buffers()
+
 	mut alive := true
 	for alive {
 		ev := sdl2.Event{}
@@ -33,9 +48,136 @@ fn main() {
 				}
 			}
 		}
+
+		C.glClearColor(0.5, 0.0, 0.3, 1.0)
+		C.glClear(C.GL_COLOR_BUFFER_BIT | C.GL_DEPTH_BUFFER_BIT | C.GL_STENCIL_BUFFER_BIT)
+		state.draw_quad()
+		//state.draw_triangle()
+		C.SDL_GL_SwapWindow(window)
 	}
 
     C.SDL_GL_DeleteContext(gl_context)
     C.SDL_DestroyWindow(window)
     C.SDL_Quit()
+}
+
+fn (state mut AppState) create_shader() {
+	// vertex shader
+	vert := C.glCreateShader(C.GL_VERTEX_SHADER)
+	vert_src := '#version 150\nin vec2 LVertexPos2D; void main() { gl_Position = vec4(LVertexPos2D.x, LVertexPos2D.y, 0, 1); }'
+	C.glShaderSource(vert, 1, &vert_src.str, 0)
+	C.glCompileShader(vert)
+	if gl.shader_compile_status(vert) == 0 {
+		log := gl.shader_info_log(vert)
+		println('shader $vert compilation failed')
+		println(log)
+		exit(1)
+	}
+
+	// fragment shader
+	frag := C.glCreateShader(C.GL_FRAGMENT_SHADER)
+	frag_src := '#version 150\nout vec4 LFragment; void main() { LFragment = vec4(1.0, 1.0, 1.0, 1.0); }'
+	C.glShaderSource(frag, 1, &frag_src.str, 0)
+	C.glCompileShader(frag)
+	if gl.shader_compile_status(frag) == 0 {
+		log := gl.shader_info_log(frag)
+		println('fragment $frag shader compilation failed')
+		println(log)
+		exit(1)
+	}
+
+	// link shaders
+	shader_program := C.glCreateProgram()
+	C.glAttachShader(shader_program, vert)
+	C.glAttachShader(shader_program, frag)
+	C.glLinkProgram(shader_program)
+
+	// check for linking errors
+	success := gl.get_program_link_status(shader_program)
+	if success == 0 {
+		log := gl.program_info_log(shader_program)
+		println('shader compilation failed')
+		println('vertex source = $vert_src')
+		println('fragment source = $frag_src')
+		println(log)
+		exit(1)
+	}
+
+	state.program = shader_program
+	state.vert_pos_loc = C.glGetAttribLocation(state.program, 'LVertexPos2D')
+	if state.vert_pos_loc == -1 {
+		println('LVertexPos2D is not a valid glsl program variable!')
+		exit(1)
+	}
+}
+
+fn (state mut AppState) create_buffers() {
+	vertex_data := [
+		-0.5, -0.5,
+		0.5, -0.5,
+		0.5,  0.5,
+		-0.5,  0.5
+	]
+	index_data := [
+		u32(0), 1, 2, 3
+	]
+
+	state.vbo = gl.gen_buffer()
+	C.glBindBuffer(C.GL_ARRAY_BUFFER, state.vbo)
+	C.glBufferData(C.GL_ARRAY_BUFFER, vertex_data.len * sizeof(f32), vertex_data.data, C.GL_STATIC_DRAW)
+
+	state.ibo = gl.gen_buffer()
+	C.glBindBuffer(C.GL_ELEMENT_ARRAY_BUFFER, state.ibo)
+	C.glBufferData(C.GL_ELEMENT_ARRAY_BUFFER, index_data.len * sizeof(u32), index_data.data, C.GL_STATIC_DRAW)
+}
+
+fn (state AppState) draw_quad() {
+	// Bind program
+	C.glUseProgram(state.program)
+
+	// Enable vertex position
+	C.glEnableVertexAttribArray(state.vert_pos_loc)
+
+	// Set vertex data
+	C.glBindBuffer(C.GL_ARRAY_BUFFER, state.vbo)
+	C.glVertexAttribPointer(state.vert_pos_loc, 2, C.GL_FLOAT, C.GL_FALSE, 2 * sizeof(f32), C.NULL)
+
+	// Set index data and render
+	C.glBindBuffer(C.GL_ELEMENT_ARRAY_BUFFER, state.ibo)
+	C.glDrawElements(C.GL_TRIANGLE_FAN, 4, C.GL_UNSIGNED_INT, C.NULL)
+
+	// Disable vertex position
+	C.glDisableVertexAttribArray(state.vert_pos_loc)
+
+	// Unbind program
+	C.glUseProgram(C.NULL)
+}
+
+fn (state mut AppState) draw_triangle() {
+	vertices := [
+		-1.0, 0.0,
+		-1.0, 1.0,
+		0.3,  1.0
+	]
+	index_data := [
+		u32(0), 1, 2
+	]
+
+	if state.vao == 0 {
+		state.vao = gl.gen_vertex_array()
+		state.tri_vbo = gl.gen_buffer()
+		C.glBindBuffer(C.GL_ARRAY_BUFFER, state.tri_vbo)
+		C.glBufferData(C.GL_ARRAY_BUFFER, vertices.len * sizeof(f32), vertices.data, C.GL_STATIC_DRAW)
+	}
+
+	C.glUseProgram(state.program)
+
+	C.glBindVertexArray(state.vao)
+	C.glBindBuffer(C.GL_ARRAY_BUFFER, state.tri_vbo)
+	C.glVertexAttribPointer(0, 2, C.GL_FLOAT, false, 2 * sizeof(f32), 0)
+	C.glEnableVertexAttribArray(0)
+	C.glDrawArrays(C.GL_TRIANGLES, 0, 3)
+
+	C.glDisableVertexAttribArray(0)
+	C.glUseProgram(C.NULL)
 }

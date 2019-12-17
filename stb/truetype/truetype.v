2679ce22fa7ff0cb71c2c2bad0f96ae10bc8c37d
Mike Desaro <mike@prime31.com>
2019-12-17 13:03:19 -0800

    more stb

---
+++ stb/truetype/truetype.v
@@ -0,0 +1,10 @@
+module truetype
+
+#flag -I @VMOD/prime31/stb/truetype/thirdparty
+
+#define STB_RECT_PACK_IMPLEMENTATION
+#include "stb_rect_pack.h"
+
+#define STB_TRUETYPE_IMPLEMENTATION 
+#include "stb_truetype.h"
+

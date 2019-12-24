f7fd9dbaab2f93cb222da82e91394d73dd8a21bb
Mike <mike@prime31.com>
2019-12-23 21:18:01 -0800

    cleanse sokol

--- sokol/sokol.v
+++ sokol/sokol.v
@@ -6,12 +6,13 @@ module sokol
 #flag darwin -fobjc-arc
 
 // METAL
-#define SOKOL_METAL
-#flag darwin -framework Metal -framework Cocoa -framework MetalKit -framework QuartzCore
+// #define SOKOL_METAL // which one? depends on import order...
+// #flag -DSOKOL_METAL
+// #flag darwin -framework Metal -framework Cocoa -framework MetalKit -framework QuartzCore
 
 // OPENGL
-// #define SOKOL_GLCORE33
-// #flag @VMOD/prime31/sokol/thirdparty/flextgl/flextGL.o
-// #flag darwin -framework OpenGL -framework Cocoa -framework QuartzCore
+// #define SOKOL_GLCORE33 // which one? depends on import order...
+#flag -DSOKOL_GLCORE33
+#flag darwin -framework OpenGL -framework Cocoa -framework QuartzCore
 // this is just to quite the warnings about gl.h and gl3.h being included by Apple
 // #flag darwin -DGL_DO_NOT_WARN_IF_MULTI_GL_VERSION_HEADERS_INCLUDED

0d2b1082f19084e52a8d46d94ffd346b70ca9483
Mike <mike@prime31.com>
2020-01-10 23:55:36 -0800

    minor updates

--- v-projects/projects/sokol_tester/sapp/tex_quad_gl_tmp.v
+++ v-projects/projects/sokol_tester/sapp/tex_quad_gl_tmp.v
@@ -78,7 +78,7 @@ fn init(user_data voidptr) {
 
 fn create_image() graphics.Texture {
 	data := os.read_bytes('assets/beach.png') or { panic('file not loaded') }
-	tex := graphics.texture_load(data)
+	tex := graphics.texture_load(data, .nearest, .nearest)
 	data.free()
 	return tex
 }
--- v-projects/projects/via_tester/via_tester.v
+++ v-projects/projects/via_tester/via_tester.v
@@ -11,6 +11,14 @@ fn main() {
 
 pub fn (state &AppState) initialize(via &via.Via) {
 	println('initialize called')
+	t := via.graphics.new_texture('assets/beach.png')
+	println('t: $t')
+	t.free()
+
+	s := via.audio.new_sound('assets/skid.wav')
+	s.play(0)
+	_, name := s.get_name()
+	println('sound name: $name')
 }
 
 pub fn (state &AppState) update(via &via.Via) {

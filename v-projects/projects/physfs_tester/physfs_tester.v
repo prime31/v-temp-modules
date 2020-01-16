b8b622842960e3fc7511349e7c626ad2c1dde7f9
Mike Desaro <mike@prime31.com>
2020-01-16 12:14:17 -0800

    updates

--- v-projects/projects/physfs_tester/physfs_tester.v
+++ v-projects/projects/physfs_tester/physfs_tester.v
@@ -1,5 +1,6 @@
 import via.libs.physfs
 import via.libs.sdl2
+import via.utils
 import os
 
 
@@ -57,13 +58,13 @@ fn enum_callback(data voidptr, dir byteptr, fname byteptr) int {
 }
 
 fn read_txt_file() {
-	fp := physfs.open_read(c'assets/json.json')
+	fp := physfs.open_read('assets/json.json')
 	// fp := physfs.open_read(charptr('assets/json.json'.str))
 	len := physfs.file_length(fp)
 	println('json file len=$len')
 
 	// buf := [len]byte // doesnt work
-	buf := [`0`].repeat(int(len))
+	buf := utils.make_array<int>(int(len), int(len))
 	physfs.read_bytes(fp, buf.data, u64(len))
 	println('file contents: ${tos(buf.data, buf.len)}')
 	physfs.close(fp)

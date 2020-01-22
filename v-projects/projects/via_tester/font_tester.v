d3abb65ab2376756b32710165eb769680d09ce35
Mike <mike@prime31.com>
2020-01-21 22:55:19 -0800

    tester

--- v-projects/projects/via_tester/font_tester.v
+++ v-projects/projects/via_tester/font_tester.v
@@ -1,11 +1,14 @@
 import via
 import via.math
 import via.graphics
+import via.fonts
 import via.libs.imgui
 
 struct AppState {
 mut:
 	batch &graphics.QuadBatch = &graphics.QuadBatch(0)
+	font &fonts.FontStash = &fonts.FontStash(0)
+	proggy int
 }
 
 fn main() {
@@ -15,9 +18,18 @@ fn main() {
 
 pub fn (state mut AppState) initialize(via &via.Via) {
 	state.batch = graphics.quadbatch(2000)
+	state.font = via.g.new_fontstash(512, 512)
+	state.proggy = state.font.add_font('assets/ProggyTiny.ttf')
+
+	state.font.draw_text(0, 0, 'farting on the bed')
+	state.font.set_color(math.color_blue_violet())
+	state.font.draw_text(0, 20, 'in your sleep')
+	state.font.iter_text(0, 20, 'in your sleep you fool')
 }
 
-pub fn (state mut AppState) update(via &via.Via) {}
+pub fn (state mut AppState) update(via &via.Via) {
+	state.font.draw_text(0, 20, 'in your sleep you fool')
+}
 
 pub fn (state mut AppState) draw(via &via.Via) {
 	pass_action := via.g.new_clear_pass(0.5, 0.4, 0.6, 1.0)

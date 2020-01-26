77c94c786c5cbee0d43a64d56a36ec9e4fad4bf4
Mike <mike@prime31.com>
2020-01-25 23:06:23 -0800

    bump

--- v-projects/projects/via_tester/offscreen.v
+++ v-projects/projects/via_tester/offscreen.v
@@ -9,11 +9,15 @@ mut:
 	batch &graphics.QuadBatch = &graphics.QuadBatch(0)
 	offscreen_pass graphics.OffScreenPass
 	rot f32
+	x f32
+	y f32
 }
 
 fn main() {
 	state := AppState{}
-	via.run(via.ViaConfig{}, mut state)
+	via.run(via.ViaConfig{
+		imgui_enabled: true
+	}, mut state)
 }
 
 pub fn (state mut AppState) initialize(via &via.Via) {
@@ -26,19 +30,23 @@ pub fn (state mut AppState) initialize(via &via.Via) {
 
 pub fn (state mut AppState) update(via &via.Via) {
 	state.rot += 0.5
+	igSliderFloat('x'.str, &state.x, -100, 100, C.NULL, 1)
+	igSliderFloat('y'.str, &state.y, -100, 100, C.NULL, 1)
 }
 
 pub fn (state mut AppState) draw(via &via.Via) {
 	pass_action := via.g.make_clear_pass(0.7, 0.4, 0.8, 1.0)
 	w, h := via.win.get_drawable_size()
-	trans_mat := math.mat32_ortho_off_center(w, h)
+
 	// render upside-down for the offscreen buffers
-	os_trans_mat := math.mat32_ortho_off_center(256, -256)
+	os_proj_mat := math.mat32_ortho_off_center(256, -256)
+	// TODO: shouldnt this be translation * projection?
+	cam_translated := os_proj_mat * math.mat32_translate(-state.x, -state.y)
 
 	state.offscreen_pass.begin()
 	sg_apply_pipeline(via.g.get_default_pipeline().pip)
 
-	state.batch.begin(os_trans_mat)
+	state.batch.begin(cam_translated)
 	state.batch.draw_q(state.atlas.tex, state.atlas.get_quad('adventurer-run-04'), {x: 0, y: 0, sx: 1, sy: 1})
 	state.batch.draw_q(state.atlas.tex, state.atlas.get_quad('adventurer-run-03'), {x: -50, y: 50, sx: 1, sy: 1})
 	state.batch.draw_q(state.atlas.tex, state.atlas.get_quad('adventurer-run-02'), {x: -100, y: -50, sx: 1, sy: 1})
@@ -49,15 +57,18 @@ pub fn (state mut AppState) draw(via &via.Via) {
 	sg_begin_default_pass(&pass_action, w, h)
 	sg_apply_pipeline(via.g.get_default_pipeline().pip)
 
-	state.batch.begin(trans_mat)
-	state.batch.draw(state.offscreen_pass.color_tex, {x:-w/2+128*3 y:0 sx:3 sy:3 ox:128 oy:128})
-	state.batch.draw(state.offscreen_pass.color_tex, {x:w/2-128, y:-h/2+128 rot:state.rot sx:1, sy:1, ox:128, oy:128})
-	state.batch.draw(state.offscreen_pass.color_tex, {x:w/2-128*2, y:h/2-128*2, sx:2, sy:2, ox:128, oy:128})
+	fs_trans_mat := math.mat32_ortho(w, h)
+	state.batch.begin(fs_trans_mat)
+	// state.batch.draw(state.offscreen_pass.color_tex, {x:-w/2+128*3 y:0 sx:3 sy:3 ox:128 oy:128})
+	// state.batch.draw(state.offscreen_pass.color_tex, {x:w/2-128, y:-h/2+128 rot:state.rot sx:1, sy:1, ox:128, oy:128})
+	// state.batch.draw(state.offscreen_pass.color_tex, {x:w/2-128*2, y:h/2-128*2, sx:2, sy:2, ox:128, oy:128})
 
-	state.batch.draw_q(state.atlas.tex, state.atlas.get_quad('adventurer-run-04'), {x: 0, y: 0, sx: 1, sy: 1})
-	state.batch.draw_q(state.atlas.tex, state.atlas.get_quad('adventurer-run-04'), {x: 0, y: 0, sx: 4, sy: 4})
-	state.batch.draw_q(state.atlas.tex, state.atlas.get_quad('adventurer-run-03'), {x: -100, y: 100, sx: 4, sy: 4})
-	state.batch.draw_q(state.atlas.tex, state.atlas.get_quad('adventurer-run-02'), {x: -100, y: -100, sx: 4, sy: 4})
+	// state.batch.draw_q(state.atlas.tex, state.atlas.get_quad('adventurer-run-04'), {x: 0, y: 0, sx: 1, sy: 1})
+	// state.batch.draw_q(state.atlas.tex, state.atlas.get_quad('adventurer-run-04'), {x: 0, y: 0, sx: 4, sy: 4})
+	// state.batch.draw_q(state.atlas.tex, state.atlas.get_quad('adventurer-run-03'), {x: -100, y: 100, sx: 4, sy: 4})
+	// state.batch.draw_q(state.atlas.tex, state.atlas.get_quad('adventurer-run-02'), {x: -100, y: -100, sx: 4, sy: 4})
+
+	state.batch.draw(state.offscreen_pass.color_tex, state.offscreen_pass.get_pixel_perfect_config(w, h))
 	state.batch.end()
 
 	sg_end_pass()

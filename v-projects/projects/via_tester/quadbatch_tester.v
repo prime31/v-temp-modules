5e25a3839fbd15f0e75091e5c4483295082eb9d5
Mike Desaro <mike@prime31.com>
2020-02-05 17:11:49 -0800

    move to using built-in batches

--- v-projects/projects/via_tester/quadbatch_tester.v
+++ v-projects/projects/via_tester/quadbatch_tester.v
@@ -6,7 +6,6 @@ import via.libs.imgui
 struct AppState {
 mut:
 	atlas graphics.TextureAtlas
-	batch &graphics.QuadBatch = &graphics.QuadBatch(0)
 	beach_tex graphics.Texture
 	dude_tex graphics.Texture
 }
@@ -20,25 +19,24 @@ pub fn (state mut AppState) initialize() {
 	state.beach_tex = graphics.new_texture('assets/beach.png')
 	state.dude_tex = graphics.new_texture('assets/dude.png')
 	state.atlas = graphics.new_texture_atlas('assets/adventurer.atlas')
-	state.batch = graphics.quadbatch(2000)
 }
 
-pub fn (state mut AppState) update() {}
+pub fn (state &AppState) update() {}
 
-pub fn (state mut AppState) draw() {
+pub fn (state &AppState) draw() {
 	graphics.begin_default_pass({color:math.color_from_floats(0.5, 0.4, 0.8, 1.0)}, {})
 
 	names := state.atlas.get_names()
 
-	state.batch.draw_q(state.atlas.tex, state.atlas.get_quad(math.choose_arr(names)), {x: 0, y: -80, sx: 2, sy: 2, ox: 25, oy: 18.5})
-	state.batch.draw_q(state.atlas.tex, state.atlas.get_quad(math.choose_arr(names)), {x: 0, y: -150, sx: 2, sy: 2, rot: 90, ox: 25, oy: 18.5})
-	state.batch.draw(state.beach_tex, {x: 0, y: 0})
-	state.batch.draw(state.beach_tex, {x: -100, y: -20})
-	state.batch.draw(state.beach_tex, {x: -200, y: -40, rot: 45})
-	state.batch.draw_q(state.atlas.tex, state.atlas.get_quad('adventurer-run-04'), {x: -100, y: -200})
-	state.batch.draw_q(state.atlas.tex, state.atlas.get_quad('adventurer-run-03'), {x: -100, y: -250})
-	state.batch.draw_q(state.atlas.tex, state.atlas.get_quad('adventurer-run-02'), {x: -100, y: -300})
-	state.batch.end()
+	mut batch := graphics.g.quad_batch
+	batch.draw_q(state.atlas.tex, state.atlas.get_quad(math.choose_arr(names)), {x: 0, y: -80, sx: 2, sy: 2, ox: 25, oy: 18.5})
+	batch.draw_q(state.atlas.tex, state.atlas.get_quad(math.choose_arr(names)), {x: 0, y: -150, sx: 2, sy: 2, rot: 90, ox: 25, oy: 18.5})
+	batch.draw(state.beach_tex, {x: 0, y: 0})
+	batch.draw(state.beach_tex, {x: -100, y: -20})
+	batch.draw(state.beach_tex, {x: -200, y: -40, rot: 45})
+	batch.draw_q(state.atlas.tex, state.atlas.get_quad('adventurer-run-04'), {x: -100, y: -200})
+	batch.draw_q(state.atlas.tex, state.atlas.get_quad('adventurer-run-03'), {x: -100, y: -250})
+	batch.draw_q(state.atlas.tex, state.atlas.get_quad('adventurer-run-02'), {x: -100, y: -300})
 
 	graphics.end_pass()
 }
--- v-projects/projects/via_tester/tribatch_tester.v
+++ v-projects/projects/via_tester/tribatch_tester.v
@@ -3,19 +3,14 @@ import via.math
 import via.graphics
 import via.libs.imgui
 
-struct AppState {
-mut:
-	batch &graphics.TriangleBatch = &graphics.TriangleBatch(0)
-}
+struct AppState {}
 
 fn main() {
 	state := AppState{}
 	via.run(via.ViaConfig{}, mut state)
 }
 
-pub fn (state mut AppState) initialize() {
-	state.batch = graphics.trianglebatch(2000)
-}
+pub fn (state mut AppState) initialize() {}
 
 pub fn (state mut AppState) update() {}
 
@@ -23,7 +18,6 @@ pub fn (state mut AppState) draw() {
 	graphics.begin_default_pass({color:math.color_from_floats(0.5, 0.4, 0.8, 1.0)}, {})
 
 	mut tb := graphics.g.tri_batch
-
 	tb.draw_triangle(200, 200, 200, 300, 400, 200, {color:math.color_red()})
 	tb.draw_rectangle(-100, -300, 200, 100, {x:0})
 	tb.draw_circle(-300, 300, 75, 6, {color:math.color_orange()})

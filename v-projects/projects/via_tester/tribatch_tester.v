ca056d78ddfa8c3262229bc23c16f2ac89d500ef
Mike <mike@prime31.com>
2020-02-11 21:17:26 -0800

    bumps

--- v-projects/projects/via_tester/tribatch_tester.v
+++ v-projects/projects/via_tester/tribatch_tester.v
@@ -19,10 +19,9 @@ pub fn (state mut AppState) draw() {
 
 	mut tb := graphics.tribatch()
 	tb.draw_triangle(200, 200, 200, 300, 400, 200, {color:math.color_red()})
-	tb.draw_rectangle(400, 300, 200, 100, {x:0})
-	tb.draw_circle(300, 300, 75, 6, {color:math.color_orange()})
-	tb.draw_circle(400, 500, 75, 16, {x:0})
-	tb.draw_polygon([math.Vec2{500, 200}, math.Vec2{600, 100}, math.Vec2{700, 250}, math.Vec2{550, 400}, math.Vec2{450, 300}]!, {x:0 color:math.color_blue()})
-
+	tb.draw_rectangle(200, 100, {x:150 y:100})
+	tb.draw_circle(75, 6, {x:100 y:600 color:math.color_orange()})
+	tb.draw_circle(75, 16, {x:400 y:500})
+	tb.draw_polygon([math.Vec2{500, 200}, math.Vec2{600, 100}, math.Vec2{700, 250}, math.Vec2{550, 400}, math.Vec2{450, 300}]!, {x:200 y:200 color:math.color_blue()})
 	graphics.end_pass()
 }
---
+++ v-projects/projects/via_tester/tribatch_transform.v
@@ -0,0 +1,29 @@
+import via
+import via.math
+import via.time
+import via.graphics
+import via.libs.imgui
+
+struct AppState {}
+
+fn main() {
+	state := AppState{}
+	via.run(via.ViaConfig{}, mut state)
+}
+
+pub fn (state mut AppState) initialize() {}
+
+pub fn (state mut AppState) update() {}
+
+pub fn (state mut AppState) draw() {
+	graphics.begin_pass({color:math.color_from_floats(0.5, 0.4, 0.6, 1.0)})
+
+	mut tb := graphics.tribatch()
+	tb.draw_triangle(-50, 50, 0, -170, 50, 50, {x:800 y:600 rot:-f32(time.ticks() / 12) color:math.color_red()})
+	tb.draw_rectangle(200, 100, {x:150 y:150 rot:-f32(time.ticks() / 10)})
+	tb.draw_circle(75, 6, {x:512 y:384 rot:f32(time.ticks() / 10) color:math.color_orange()})
+	tb.draw_circle(75, 16, {x:800 y:150})
+	tb.draw_polygon([math.Vec2{-50, -50}, math.Vec2{0, -100}, math.Vec2{100, 50}, math.Vec2{70, 100}, math.Vec2{0, 150}]!, {x:150 y:600 rot:f32(time.ticks() / 5) color:math.color_blue()})
+
+	graphics.end_pass()
+}

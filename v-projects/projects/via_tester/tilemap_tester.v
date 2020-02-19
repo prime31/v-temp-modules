0619fca2cec98eb42ff5db9e2bffa958d754f4cb
Mike Desaro <mike@prime31.com>
2020-02-18 16:21:22 -0800

    jsmn map parser

--- v-projects/projects/via_tester/tilemap_tester.v
+++ v-projects/projects/via_tester/tilemap_tester.v
@@ -1,5 +1,7 @@
 import via
 import via.math
+import via.time
+import via.debug
 import via.window
 import via.graphics
 import via.filesystem
@@ -18,7 +20,7 @@ fn main() {
 		layer_batch: 0
 	}
 
-	via.run(via.ViaConfig{
+	via.run({
 		win_highdpi: false
 		resolution_policy: .show_all_pixel_perfect
 		design_width: 640
@@ -30,21 +32,35 @@ fn main() {
 
 pub fn (state mut AppState) initialize() {
 	filesystem.mount('assets', '', true)
-	map := json.decode(tilemap.Map, filesystem.read_text_c(c'assets/platformer.json')) or { panic('could not load map') }
-	state.renderer = tilemap.maprenderer(map)
-	state.layer_batch = state.renderer.tilelayer_atlasbatch(map.tile_layers[0])
 
-	t1 := map.tilesets[0].tileset_tile(2)
-	for p in t1.props {
-		println('$p.key -> $p.value')
-	}
+	js_bytes := filesystem.read_bytes_c(c'assets/platformer.json')
+	now0s := time.now()
+	map0 := tilemap.load(js_bytes)
+	now0e := time.now()
+
+	js_txt := filesystem.read_text_c(c'assets/platformer.json')
+	now1s := time.now()
+	map := json.decode(tilemap.Map, js_txt) or { panic('could not load map') }
+	now1e := time.now()
+
+	state.renderer = tilemap.maprenderer(map0)
+	state.layer_batch = state.renderer.tilelayer_atlasbatch(map0.tile_layers[0])
+
+	// t1 := map0.tilesets[0].tileset_tile(2)
+	// for p in t1.props {
+	// 	println('$p.key -> $p.value')
+	// }
+
+	jsmnt := now0e - now0s
+	jst := now1e - now1s
+	debug.warn('-- jsmn: ${f32(jsmnt) / 1000}, jst: ${f32(jst) / 1000} --')
 }
 
 pub fn (state &AppState) update() {}
 
 pub fn (state mut AppState) draw() {
 	graphics.begin_pass({color:math.color_from_floats(0.3, 0.1, 0.4, 1.0)})
-	//state.renderer.render()
+	// state.renderer.render()
 	state.layer_batch.draw()
 	graphics.end_pass()
 }
